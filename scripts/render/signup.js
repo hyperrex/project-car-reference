b3e296cec31dd80c588c77400de521b1acc51274
hyperrex <mrwendell@gmail.com>
2018-11-05 15:36:57 -0800

    Create signup

---
+++ scripts/render/signup.js
@@ -0,0 +1,21 @@
+// on dom content loaded
+const signup = event => {
+  event.preventDefault();
+  const first_name = document.querySelector('#first-name').value;
+  const last_name = document.querySelector('#last-name').value;
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#sign-up-password').value;
+  const verified = document.querySelector('#verify-password').value;
+
+  if (signUpPassword !== verified) {
+    return alert('Passwords must match!');
+  }
+
+  axios
+    // .post('https://project-car-reference-api.herokuapp.com/users/', { email, password })
+    .post('http://localhost:8000/users/', { first_name, last_name, email, password })
+    .then(response => {
+      console.log(response)
+    });
+};
+document.querySelector('#sign-up-form').addEventListener('submit', signup);
