73aae72533db0f39386aed6f3930cb207ee62720
hyperrex <mrwendell@gmail.com>
2018-11-05 19:38:04 -0800

    Add icons to buttons

--- scripts/render/login.js
+++ scripts/render/login.js
@@ -1,15 +1,16 @@
-// on dom content loaded
-const tryLogin = event => {
-  event.preventDefault();
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
+document.addEventListener('DOMContentLoaded', () => {
+  const tryLogin = event => {
+    event.preventDefault();
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
 
-  axios
-    // .post('https://project-car-reference-api.herokuapp.com/users/', { email, password })
-    .post('http://localhost:8000/users/login/', { email, password })
-    .then(response => {
-      localStorage.setItem('token', response.headers.authorization);
-      console.log(localStorage.getItem('token'));
-    });
-};
-document.querySelector('#login-form').addEventListener('submit', tryLogin);
+    axios
+      // .post('https://project-car-reference-api.herokuapp.com/users/', { email, password })
+      .post('http://localhost:8000/users/login/', { email, password })
+      .then(response => {
+        localStorage.setItem('token', response.headers.authorization);
+        console.log(localStorage.getItem('token'));
+      });
+  };
+  document.querySelector('#login-form').addEventListener('submit', tryLogin);
+});
