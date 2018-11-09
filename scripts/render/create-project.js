63870cf56c5e7d134363a3dbb6706bbd8e9ca18f
hyperrex <mrwendell@gmail.com>
2018-11-08 20:07:24 -0800

    Create project functions

--- scripts/render/create-project.js
+++ scripts/render/create-project.js
@@ -2,12 +2,20 @@ const createNewProject = event => {
   event.preventDefault();
   let title = document.querySelector('#project-title').value;
   let description = document.querySelector('#project-description').value;
+  let users_id = localStorage.getItem('user');
   axios
     // .post('https://project-car-reference-api.herokuapp.com/projects/', { title, description })
     .post('http://localhost:8000/projects/', { title, description })
     .then(response => {
       console.log(response);
-      viewMyProjects();
+      let projects_id = response.data[0].id;
+      axios
+        // .post('https://project-car-reference-api.herokuapp.com/userProjects/', { title, description })
+        .post('http://localhost:8000/userProject/', { users_id, projects_id })
+        .then(response => {
+          console.log(response);
+          viewMyProjects();
+        });
     });
 };
 
